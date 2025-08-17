7bd233a55e1a7e3e3f9778bbdfa82b790183b34b
MgKyaw <mgkyaw20112@gmail.com>
2025-08-17 19:43:15 +0700

    feat: Add new viewmodel ExplorerDbViewModel

---
+++ MongoDbTest/Models/ExplorerDbViewModel.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using MongoDB.Bson;
+
+namespace MongoDbTest.Models
+{
+  public class ExplorerDbViewModel
+  {
+    public string Database { get; set; }
+    public string Collection{ get; set; }
+    public BsonDocument Document { get; set; }
+    public Dictionary<string, List<string>> DatabasesAndCollections { get; set; }
+    public int Index { get; set; }
+    public long CollectionCount { get; set; }
+  }
+}
